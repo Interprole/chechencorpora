754c53678d021e5744ab87ca82e3dc68ce4285a6
Interprole <gleb.bubnov2004@mail.ru>
2024-03-03 23:03:07 +0300

    first try

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-import flask
+from flask import Flask, render_template, request, redirect, Response, session
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def base():
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    app.run(port=80, debug=False)
